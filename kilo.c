8f3d2cf44ae01c890facb64f2c917f4bc28bfe22
Dhruv Singh <dhruvsingh99@gmail.com>
2017-08-14 23:14:52 +0530

    can input stuff in terminal for now

--- kilo.c
+++ kilo.c
@@ -1,6 +1,30 @@
 #include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+
+struct termios orig_termios;
+
+void disableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios);
+}
+
+/*
+** This function is disabling automatic ECHO. So you can't see what you type.
+** c_lflag = local flag which is dumping ground for other states
+** 
+*/
+void enableRawMode() {
+    tcgetattr(STDIN_FILENO, &orig_termios);
+    atexit(disableRawMode);
+
+    struct termios raw = orig_termios;
+    raw.c_lflag &= ~(ECHO | ICANON); // ICANON makes input byte by byte
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
 
 int main() {
+    enableRawMode();
     char c;
     while ( read(STDIN_FILENO, &c, 1) == 1 && c != 'q' );
     return 0;
