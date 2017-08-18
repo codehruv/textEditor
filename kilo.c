129abf6313cf45de5c85e71eb80d71d12775ca62
Dhruv Singh <dhruvsingh99@gmail.com>
2017-08-17 20:45:55 -0400

    welcome message placed

--- kilo.c
+++ kilo.c
@@ -12,6 +12,7 @@
 /*** defines ***/
 
 #define CTRL_KEY(k) ((k) & 0x1f)
+#define KILO_VERSION "0.0.1"
 
 /*** data ***/
 
@@ -124,7 +125,23 @@ void abFree(struct abuf *ab) {
 
 void editorDrawRows(struct abuf *ab) {
 	for(int y = 0; y < E.screenrows; y++) {
-        abAppend(ab, "~", 1);
+        if( y == E.screenrows / 3) {
+            char welcome[80];
+            int welcomelen = snprintf(welcome, sizeof(welcome), 
+                "Kilo editor -- version %s", KILO_VERSION);
+            if(welcomelen > E.screencols)
+                welcomelen = E.screencols;
+            int padding = ( E.screencols - welcomelen ) / 2;
+            if(padding) {
+                abAppend(ab, "~", 1);
+                padding--;
+            }
+            while(padding--) abAppend(ab, " ", 1);
+            abAppend(ab, welcome, welcomelen);
+        }
+        else
+            abAppend(ab, "~", 1);
+        abAppend(ab, "\x1b[K", 3);
         if(y < E.screenrows - 1)
             abAppend(ab, "\r\n", 2); 
 	}
@@ -134,7 +151,6 @@ void editorRefreshScreen() {
     struct abuf ab = ABUF_INIT;
     
     abAppend(&ab, "\x1b[?25l", 6);
-    abAppend(&ab, "\x1b[2J", 4);
     abAppend(&ab, "\x1b[H", 3);
 
     editorDrawRows(&ab);
